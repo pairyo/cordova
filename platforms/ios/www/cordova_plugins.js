21f169b74ef7784869fbe885241ee7ca9c72fda6
Zhiyue Zu <zzy@Workbench.local>
2015-08-04 21:07:10 +0800

    make Pairyo more like native app

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -6,13 +6,21 @@ module.exports = [
         "clobbers": [
             "window.StatusBar"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.0.0",
-    "cordova-plugin-statusbar": "1.0.1"
+    "cordova-plugin-statusbar": "1.0.1",
+    "org.apache.cordova.splashscreen": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
